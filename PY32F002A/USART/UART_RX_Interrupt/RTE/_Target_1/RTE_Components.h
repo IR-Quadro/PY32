f86bc6d7a8eed6b136604bb9cbee264e2fafb133
Quadro <abolfazl.resalatifar@gmail.com>
2025-10-31 11:26:23 +0330

    initial commit

--- PY32F002A/USART/UART_RX_Interrupt/RTE/_Target_1/RTE_Components.h
+++ PY32F002A/USART/UART_RX_Interrupt/RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'UART_Interrupt' 
+ * Project: 'UART_RX_Interrupt' 
  * Target:  'Target 1' 
  */
 
